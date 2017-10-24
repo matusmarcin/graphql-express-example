d782919ab2ab3085a99da6c4cfe256b46f03780a
Matus Marcin <matus.marcin@gmail.com>
2017-10-24 14:44:09 +0200

    Added cors, id as string

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import cors from 'cors';
 import schema from './schema';
 
 const app = express();
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', cors(), graphqlHTTP({
   schema: schema,
   graphiql: true,
 }));
